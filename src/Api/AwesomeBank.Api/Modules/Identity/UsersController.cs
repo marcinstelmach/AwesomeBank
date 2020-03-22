8094595a3ff262537e5158b3c423fccf6f4d6343
Marcin Stelmach <marcinxd4@gmail.com>
2020-03-22 16:29:13 +0100

    Initial Commit

---
+++ src/Api/AwesomeBank.Api/Modules/Identity/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeBank.Api.Modules.Identity
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> TestAsync()
+        {
+            await Task.CompletedTask;
+            return Ok("Hello world");
+        }
+    }
+}
---
+++ tests/AwesomeBank.Api.Tests/Modules/Identity/UsersControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AwesomeBank.Api.Modules.Identity;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace AwesomeBank.Api.Tests.Modules.Identity
+{
+    public class UsersControllerTests
+    {
+        private readonly UsersController _sut;
+
+        public UsersControllerTests()
+        {
+            _sut = new UsersController();
+        }
+
+        [Fact]
+        public async Task When_Testing_Then_Returns_Ok_Object_Result_With_Hello_World_Message()
+        {
+            // Act
+            var result = await _sut.TestAsync();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            var objectResult = result.As<OkObjectResult>();
+            objectResult.Value.ToString().Should().Be("Hello world");
+        }
+    }
+}
