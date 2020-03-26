6ca6d5cda62c7da049da53526767ad0c0361eeb7
Marcin Stelmach <marcinxd4@gmail.com>
2020-03-26 22:11:57 +0100

    Fixed build

--- src/Modules/Identity/AwesomeBank.Identity.Domain/Entities/ApplicationGroup.cs
+++ src/Modules/Identity/AwesomeBank.Identity.Domain/Entities/ApplicationGroup.cs
@@ -9,7 +9,5 @@
         public string Name { get; private set; }
 
         public virtual ICollection<ApplicationGroupClaim> ApplicationGroupClaims { get; private set; }
-
-        public virtual ICollection<ApplicationGroupRole> ApplicationGroupRoles { get; private set; }
     }
 }
