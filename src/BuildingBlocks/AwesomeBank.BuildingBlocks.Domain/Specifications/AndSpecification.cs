21fe54b15a18243b30a14713ad404033798991cd
Marcin Stelmah <marcinxd4@gmail.com>
2020-04-16 20:13:06 +0200

    Added specification pattern

---
+++ src/BuildingBlocks/AwesomeBank.BuildingBlocks.Domain/Specifications/AndSpecification.cs
@@ -0,0 +1,28 @@
+﻿namespace AwesomeBank.BuildingBlocks.Domain.Specifications
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public class AndSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> _leftSpecification;
+        private readonly Specification<T> _rightSpecification;
+
+        public AndSpecification(Specification<T> leftSpecification, Specification<T> rightSpecification)
+        {
+            _leftSpecification = leftSpecification;
+            _rightSpecification = rightSpecification;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var leftExpression = _leftSpecification.ToExpression();
+            var rightExpression = _rightSpecification.ToExpression();
+
+            var rebindParameterVisitor = new ParameterReplacer(rightExpression.Parameters[0], leftExpression.Parameters[0]);
+            var right = rebindParameterVisitor.Visit(rightExpression.Body);
+            var expression = Expression.AndAlso(leftExpression.Body, right ?? throw new InvalidOperationException());
+            return Expression.Lambda<Func<T, bool>>(expression, leftExpression.Parameters);
+        }
+    }
+}
---
+++ src/BuildingBlocks/AwesomeBank.BuildingBlocks.Domain/Specifications/Specification.cs
@@ -0,0 +1,22 @@
+﻿namespace AwesomeBank.BuildingBlocks.Domain.Specifications
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public abstract class Specification<T>
+    {
+        public abstract Expression<Func<T, bool>> ToExpression();
+
+        public bool IsSatisfiedBy(T entity)
+        {
+            var predicate = ToExpression().Compile();
+            return predicate(entity);
+        }
+
+        public Specification<T> And(Specification<T> specification)
+            => new AndSpecification<T>(this, specification);
+
+        public Specification<T> Or(Specification<T> specification)
+            => new OrSpecification<T>(this, specification);
+    }
+}
